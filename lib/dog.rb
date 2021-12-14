d0a9d0171a84b406a6d126ddde47cd6f5ee99c37
smh1988-2 <seamhurley1988@gmail.com>
2021-12-14 15:28:29 -0800

    commit tree 3dd0c5e185ee46efeb919cf492111540ae58155d

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,3 +1,21 @@
 class Dog
 
+    #setters
+    def name=(name)
+        @name = name
+    end 
+
+    def breed=(breed)
+        @breed = breed
+    end 
+
+    #getters
+    def name
+        @name
+    end
+
+    def breed
+        @breed
+    end
+
 end
--- lib/person.rb
+++ lib/person.rb
@@ -1,3 +1,20 @@
 class Person
 
+    #setters
+    def name=(name)
+        @name = name
+    end 
+
+    def job=(job)
+        @job = job
+    end 
+
+    #getters
+    def name
+        @name
+    end
+
+    def job
+        @job
+    end
 end
